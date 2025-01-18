a35aa9e6d80ee7278c6a34a21d8bcbb455504488
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-01-18 16:43:33 +0530

    tailwind css properly linked and the Header Start the coding

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/**
+ * @copyright 2024 lasithadilshan
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+ */
+import Header from "./components/Header";
+
+const App = () => {
+
+    return (
+        <Header />
+    )
+
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
+/**
+ * @copyright 2024 lasithadilshan
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+/**
+ * Components
+ */
+import App from './App.jsx';
+
+
+/**
+ * CSS link
+ */
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
