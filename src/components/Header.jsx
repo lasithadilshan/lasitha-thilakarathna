939c8dff610857181785792bf1cb016905ff3fef
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-01-19 13:14:57 +0530

    header part fully completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,19 @@
  * @license Apache-2.0
  */
 
+/**
+ * Node modules
+ */
+import { useState } from "react";
+
+/**
+ * Components
+ */
+import Navbar from "./Navbar";
+
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header
       className="fixed top-0 left-0 w-full h-20 flex
@@ -27,13 +39,15 @@ const Header = () => {
         </h1>
 
         <div className="relative md:justify-self-center">
-          <button className="menu-btn md:hidden" onClick={null}>
-            <span className="material-symbols-rounded">menu</span>
+          <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
+            <span className="material-symbols-rounded">
+              {navOpen ? 'close' : 'menu'}
+            </span>
           </button>
-          Navbar
+          <Navbar navOpen={navOpen}/>
         </div>
 
-        <a href="#contact" className="">
+        <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
           Contact Me
         </a>
       </div>
