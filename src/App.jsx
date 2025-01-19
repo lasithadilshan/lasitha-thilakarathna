39bef28db9cd3fd8c849a0967a05e283255b6ce1
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-01-19 20:55:57 +0530

    55.34 coding the Hero.jsx started

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@
  * Components
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
 
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <Hero />
+            </main>
+        </>
     )
 
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+/**
+ * @copyright 2024 lasithadilshan
+ * @license Apache-2.0
+ */
+
+const Hero = () => {
+  return (
+    <section
+    id="home"
+    className="">
+        <div className="">
+            <div>
+                <div className="">
+                    <figure className="">
+                        <img 
+                        src="/images/avatar-1.jpg"
+                        width={40}
+                        height={40}
+                        alt="Lasitha Dilshan Thilakarathna"
+                        className="img-cover" 
+                        />
+                    </figure>
+
+                    <div className="">
+                        <span className="">
+                            <span className=""></span>
+                        </span>
+
+                        Available for work
+                    </div>
+                </div>
+
+                <h2 className="">
+                    Building Scalable Modern Websites for the Future
+                </h2>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
