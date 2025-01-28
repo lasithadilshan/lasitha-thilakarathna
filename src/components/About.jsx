15850d399c2dcf695563d67c95e0f4bc8e46ded3
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-01-28 22:24:47 +0530

    About Page Styling Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,17 +21,20 @@ const About = () => {
       className="section"
     >
       <div className="container">
-        <p className="">
+        <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
+        <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
         Welcome! I&apos;m Lasitha, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.
         </p>
-        <div className="">
+        
+        <div className="flex flex-wrap items-center gap-4 md:gap-7">
           {
             aboutItems.map(({ label, number}, key) => (
               <div key={key}>
-                <span className="">{number}</span>
-                <span className="">+</span>
-
-                <p className="">{label}</p>
+                <div className="flex items-center md:mb-2">
+                <span className="text-2xl font-semibold md:text-4xl">{number}</span>
+                <span className="text-sky-400 font-semibold md:text-3xl">+</span>
+              </div>
+              <p className="text-sm text-zinc-400">{label}</p>
               </div>
             ))
           }
@@ -40,9 +43,10 @@ const About = () => {
           alt="Logo"
           width={30}
           height={30}
-          className="section"
+          className="ml-auto md:w-[40px] md:h-[40px]"
           />
         </div>
+      </div>
       </div>
     </section>
   )
