819d9e2f06e9af05b097400580748e4ef7ce871b
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-02-05 22:58:40 +0530

    Enhance ProjectCard and Work components with improved styling and layout 01:51:00

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -57,7 +57,7 @@ const Work = () => {
         <h2 className="headline-2 mb-8">
           My portfolio highlights
         </h2>
-        <div className="">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,minmax(280px,_1fr))]">
           {works.map(({ imgSrc, title, tags, projectLink },
            key) => (
             <ProjectCard 
