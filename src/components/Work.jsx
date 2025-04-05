6f6aab075cb94bda45c08e836cf320e61a5aef1c
lasithadilshan <dilshantilakaratne29@gmail.com>
2025-04-05 12:26:18 +0530

    Remove outdated project images and update project details in Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,37 +10,37 @@ import ProjectCard from './ProjectCard';
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
-    tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
+    imgSrc: '/images/project-1.png',
+    title: 'Machine Learning App',
+    tags: ['ML', 'MVC', 'Development'],
+    projectLink: 'https://ld-machinelearning.streamlit.app/'
   },
   {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://pixstock-official.vercel.app/'
+    imgSrc: '/images/project-2.png',
+    title: 'Gen AI App',
+    tags: ['Generative AI', 'OpenAi', 'Development', 'gpt-4'],
+    projectLink: 'https://github.com/lasithadilshan/MscResearchProject'
   },
   {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
+    imgSrc: '/images/project-3.png',
+    title: 'CRUD',
+    tags: ['Development', 'Angular', 'CRUD'],
+    projectLink: 'https://github.com/lasithadilshan/Angular11CRUD'
   },
   {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+    imgSrc: '/images/project-4.png',
+    title: 'Music Player Android App',
+    tags: ['Mobile-design', 'Development'],
+    projectLink: 'https://github.com/lasithadilshan/DMusic'
   },
   {
-    imgSrc: '/images/project-5.jpg',
+    imgSrc: '/images/project-5.png',
     title: 'eCommerce website',
     tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+    projectLink: 'https://github.com/lasithadilshan/foodgarage'
   },
   {
-    imgSrc: '/images/project-6.jpg',
+    imgSrc: '/images/project-6.png',
     title: 'vCard Personal portfolio',
     tags: ['Web-design', 'Development'],
     projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
